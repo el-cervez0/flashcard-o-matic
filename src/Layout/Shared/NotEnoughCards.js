1e9a96ea4eb67270f78f4f76e64209ff3288e76b
gg <glenn.gomez61@gmail.com>
2023-11-29 00:42:34 -0600

    last style change

--- src/Layout/Shared/NotEnoughCards.js
+++ src/Layout/Shared/NotEnoughCards.js
@@ -3,9 +3,9 @@ import AddCardButton from "./Buttons/AddCardButton";
 
 function NotEnoughCards({ deckId }) {
     return (
-        <div style={{ marginTop: '25px' }}>
-            <h3>Not enough cards.</h3>
-            <p>You need at least 3 cards to study. There are 2 cards in the deck.</p>
+        <div style={{ marginTop: '25px' }} className="jumbotron">
+            <h3 className="display-4">Not enough cards.</h3>
+            <p className="lead">You need at least 3 cards to study. There are 2 cards in the deck.</p>
             <AddCardButton deckId={deckId}/>
         </div>
     )
